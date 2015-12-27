cb93cd87a0f79d214df1ecf583ab50145007d1a4
Niyas Mohammed A <niaz.angels@gmail.com>
2015-12-27 13:42:17 +0000

    Test code for embedding ip camera to be integrated into iot_wheelchair

---
+++ server.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var fs = require("fs");
+
+
+http.createServer(function(request, response) {
+
+	if(request.url === "/index"){
+		sendFileContent(response, "index.html", "text/html");
+	}
+	else if(request.url === "/"){
+		sendFileContent(response, "Index.html", "text/html");
+	}
+	else if(/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+	}
+	else if(/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/css");
+	}
+	else{
+		console.log("Requested URL is: " + request.url);
+		response.end();
+	}
+}).listen(3000);
+function sendFileContent(response, fileName, contentType){
+	fs.readFile(fileName, function(err, data){
+		if(err){
+			response.writeHead(404);
+			response.write("Not Found!");
+		}
+		else{
+			response.writeHead(200, {'Content-Type': contentType});
+			response.write(data);
+		}
+		response.end();
+	});
+}
+
